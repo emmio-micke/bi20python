595909e5d7d06055e0dacc8d5730c53e779c9802
Mikael Olsson <mikael.olsson@emmio.se>
2021-03-26 08:55:23 +0100

    Added number of guesses to guess number game

--- code/04 - guess_number/guess.py
+++ code/04 - guess_number/guess.py
@@ -3,9 +3,11 @@ from random import randint
 guess_number = randint(0, 100)
 
 continue_game = True
+guesses = 0
 
 while continue_game:
     answer = int(input("Gissa nummer: "))
+    guesses += 1
 
     if answer < guess_number:
         print("Du gissade för lågt!")
@@ -14,7 +16,7 @@ while continue_game:
         print("Du gissade för högt!")
 
     if answer == guess_number:
-        print("Du gissade rätt!")
+        print(f"Du gissade rätt! Du behövde {guesses} gissningar.")
         continue_game = False
 
 print("Spelet är slut!")
